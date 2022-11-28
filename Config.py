476995afd57019d64e5adac13b1ded7f42acf481
EmmanuelEngineer <e.coppola37@studenti.unisa.it>
2022-11-28 19:13:19 +0100

    cosine and cityblock

--- Config.py
+++ Config.py
@@ -7,7 +7,7 @@ class Debug:
     active = False
 
 
-version = 4
+version = 5
 image_dataset = "/home/emmavico/Documents/Tirocinio/biwi_rgb_renamed/" if not Debug.active else Test.image_dataset
 working_directory = "./workingdirectory/" if not Debug.active else Test.working_directory
 n_of_threads = 4
